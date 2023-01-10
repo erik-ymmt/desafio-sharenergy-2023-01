5902981cfeeefcf3300041f876f6a2606415d021
Erik Yamamoto <erik.ymmt@gmail.com>
2023-01-10 10:18:02 -0300

    chore(frontend): browser router setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import ClientsList from './pages/ClientsList';
+import HttpCat from './pages/HttpCat';
+import RandomDog from './pages/RandomDog';
+import Users from './pages/Users';
 
 function App(): JSX.Element {
   return (
     <div className="App">
-
+      <Routes>
+        <Route path='/users' element={<Users />} />
+        <Route path='/cats' element={<HttpCat />} />
+        <Route path='/dogs' element={<RandomDog />} />
+        <Route path='/clients' element={<ClientsList />} />
+      </Routes>
     </div>
   );
 }
