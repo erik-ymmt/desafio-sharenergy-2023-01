454a1622c6d754658248a581d22774b458f272ce
Erik Yamamoto <erik.ymmt@gmail.com>
2023-01-14 22:28:49 -0300

    feat(frontend): add authentication to all routes

--- frontend/src/pages/RandomDog.tsx
+++ frontend/src/pages/RandomDog.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Header from '../components/Header';
 
 function RandomDog(): JSX.Element {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const loggedIn = localStorage.getItem('se_rememberme');
+    if (loggedIn !== 'true') navigate('/');
+  }, []);
   return (
     <>
       <Header />
