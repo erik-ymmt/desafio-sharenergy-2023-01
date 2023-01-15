1877f9fdb85a6f1fb742b65850833fcef5fb03c0
Erik Yamamoto <erik.ymmt@gmail.com>
2023-01-15 18:13:16 -0300

    feat(frontend): create client list and card component

--- frontend/src/components/ClientCard.tsx
+++ frontend/src/components/ClientCard.tsx
@@ -1,8 +1,32 @@
 import React from 'react';
+import { IClient } from '../pages/ClientsList';
 
-function ClientCard(): JSX.Element {
+interface IClientCardProps {
+  clientData: IClient
+}
+
+function ClientCard({ clientData }: IClientCardProps): JSX.Element {
+  const editClient = (id: string): void => {
+    console.log(id);
+  };
+  const deleteClient = (id: string): void => {
+    console.log(id);
+  };
+  const { _id, name, email, phoneNumber, address, cpf } = clientData;
   return (
-    <div>ClientCard</div>
+    <div>
+      <div>
+        <p>Name: {name}</p>
+        <p>Email: {email}</p>
+        <p>Phone: {phoneNumber}</p>
+        <p>Address: {address}</p>
+        <p>CPF: {cpf}</p>
+      </div>
+      <div>
+        <button onClick={() => { editClient(_id); }}>edit</button>
+        <button onClick={() => { deleteClient(_id); }}>delete</button>
+      </div>
+    </div>
   );
 }
 
--- frontend/src/pages/ClientsList.tsx
+++ frontend/src/pages/ClientsList.tsx
@@ -1,19 +1,43 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import ClientCard from '../components/ClientCard';
 import Header from '../components/Header';
+
+export interface IClient {
+  _id: string
+  name: string
+  email: string
+  phoneNumber: string
+  address: string
+  cpf: string
+}
 
 function ClientsList(): JSX.Element {
   const navigate = useNavigate();
+  const [clientsList, setClientsList] = useState([]);
+
+  const getClients = async (): Promise<void> => {
+    const response = await fetch('http://localhost:3001/clients');
+    const result = await response.json();
+    setClientsList(result);
+  };
 
   useEffect(() => {
     const loggedIn = localStorage.getItem('se_rememberme');
     if (loggedIn !== 'true') navigate('/');
+
+    void getClients();
   }, []);
 
   return (
     <>
       <Header />
       <h2>ClientsList</h2>
+      <div>
+        {clientsList.map((client: IClient) => (
+          <ClientCard key={client._id} clientData={client} />
+        ))}
+      </div>
     </>
   );
 }
