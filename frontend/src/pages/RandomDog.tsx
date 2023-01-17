bae399cc9f1734d1663e20f2329cab5d566dc672
Erik Yamamoto <erik.ymmt@gmail.com>
2023-01-16 22:56:11 -0300

    feat(frontend): add random dog style

--- frontend/src/pages/RandomDog.tsx
+++ frontend/src/pages/RandomDog.tsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Header from '../components/Header';
 
+import { BiRefresh } from 'react-icons/bi';
+
 function RandomDog(): JSX.Element {
   const navigate = useNavigate();
   const [dogImg, setDogImg] = useState('');
@@ -19,12 +21,26 @@ function RandomDog(): JSX.Element {
   }, []);
 
   return (
-    <>
+    <div className='bg-se_bg h-screen'>
       <Header />
-      <h2>RandomDog</h2>
-      <button onClick={getRandomDog}>New Dog</button>
-      <img src={`https://random.dog/${dogImg}`} alt="" />
-    </>
+      <div className='flex flex-col items-center'>
+        <h2 className='mt-8 mb-4 text-2xl font-semibold text-se_green'>
+          RandomDog
+        </h2>
+        <button
+          className='bg-se_green py-1 px-2 w-52 h-10 flex justify-center items-center rounded-lg text-white gap-2 text-lg mb-8'
+          onClick={getRandomDog}
+          >
+          New Dog
+          <BiRefresh />
+        </button>
+        <img
+          className='h-96 border-4 border-se_green'
+          src={`https://random.dog/${dogImg}`}
+          alt='random dog'
+        />
+      </div>
+    </div>
   );
 }
 
