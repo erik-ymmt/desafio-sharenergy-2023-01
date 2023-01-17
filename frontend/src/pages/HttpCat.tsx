2f7e070477659f2d4fcfcef949417e845dab979c
Erik Yamamoto <erik.ymmt@gmail.com>
2023-01-16 22:49:33 -0300

    feat(frontend): add random cat style

--- frontend/src/pages/HttpCat.tsx
+++ frontend/src/pages/HttpCat.tsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Header from '../components/Header';
 
+import { IoMdSearch } from 'react-icons/io';
+
 function HttpCat(): JSX.Element {
   const navigate = useNavigate();
   const [searchBarText, setSearchBarText] = useState('');
@@ -13,21 +15,33 @@ function HttpCat(): JSX.Element {
   }, []);
 
   return (
-    <>
+    <div className='bg-se_bg h-screen'>
       <Header />
-      <h2>HttpCat</h2>
-      <form>
-        <input
-          type="text"
-          placeholder="Type a HTTP status code e.g. 400"
-          onChange={({ target: { value } }) => { setSearchBarText(value); }}
+      <div className='flex flex-col items-center'>
+        <h2 className='mt-8 mb-4 text-2xl font-semibold text-se_green'>
+          HttpCat
+        </h2>
+        <form className='flex gap-4 mb-8'>
+          <input
+            className='rounded-lg border-se_green border-2 px-4 w-96'
+            type="text"
+            placeholder="Type a HTTP status code e.g. 400"
+            onChange={({ target: { value } }) => { setSearchBarText(value); }}
+          />
+          <button
+            className='bg-se_green py-1 px-2 w-10 h-10 flex justify-center items-center rounded-lg text-white'
+            onClick={(e) => { e.preventDefault(); setStatusCode(searchBarText); }}
+          >
+            <IoMdSearch size='100%'/>
+          </button>
+        </form>
+        <img
+          className='h-96'
+          src={`https://http.cat/${statusCode}.jpg`}
+          alt={`${statusCode} cat`}
         />
-        <button onClick={(e) => { e.preventDefault(); setStatusCode(searchBarText); }}>
-          Search
-        </button>
-      </form>
-      <img src={`https://http.cat/${statusCode}.jpg`} alt={`${statusCode} cat`} />
-    </>
+      </div>
+    </div>
   );
 }
 
